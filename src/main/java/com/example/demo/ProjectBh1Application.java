d3ba47e5b64842f33d3e88307681c64de7018aae
mkelly1 <mkelly@his.com>
2019-08-30 14:19:55 -0400

    Bull Horn 1 commit 1

---
+++ src/main/java/com/example/demo/ProjectBh1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBh1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBh1Application.class, args);
+	}
+
+}
